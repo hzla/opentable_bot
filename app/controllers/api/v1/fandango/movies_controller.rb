2fd80a14be748e9a437e78ddc6bfab83e41b889d
hzla00 <neohzla@gmail.com>
2014-10-27 20:19:09 -0700

    added fandango methods for calling and sorting movies

--- app/controllers/api/v1/fandango/movies_controller.rb
+++ app/controllers/api/v1/fandango/movies_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::Fandango::MoviesController < ApplicationController
 
   def index
-  	render json: {hi: true}
+  	lat = params[:lat]
+  	lng = params[:lng]
+  	start_time = params[:start_time]
+  	movies = FandangoTools.movie_recs lat, lng, start_time
+  	#sample call
+
+
+  	render json: {movies: movies}
   end
 
 end
